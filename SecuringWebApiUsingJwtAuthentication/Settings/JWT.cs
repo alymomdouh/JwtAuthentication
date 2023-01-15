967289283f799672d3b3f26b979a1e086ed22187
alyxmomdouh@gmail.com <alyxmomdouh@gmail.com>
2023-01-16 00:22:05 +0200

    read data from our previously created JWT Section of appsettings.json using the IOptions feature of ASP.NET Core.

---
+++ SecuringWebApiUsingJwtAuthentication/Settings/JWT.cs
@@ -0,0 +1,14 @@
+﻿namespace SecuringWebApiUsingJwtAuthentication.Settings
+{
+    public class JWT
+    {
+        public string Key { get; set; }
+        //Key – The Super Secret Key that will be used for Encryption.You can move this somewhere else for extra security.
+        public string Issuer { get; set; }
+        //Issuer – identifies the principal that issued the JWT.
+        public string Audience { get; set; }
+        //Audience – identifies the recipients that the JWT is intended for.
+        public double DurationInMinutes { get; set; }
+        //DurationInMinutes – Defines the Minutes the generated JWT will remain valid.
+    }
+}
