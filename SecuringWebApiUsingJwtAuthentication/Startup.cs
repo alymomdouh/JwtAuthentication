139c6492026aabeba0ad2b059e76b81f1fe12317
alyxmomdouh@gmail.com <alyxmomdouh@gmail.com>
2023-01-16 01:26:58 +0200

    add jwt config and interface and register in startup

--- SecuringWebApiUsingJwtAuthentication/Startup.cs
+++ SecuringWebApiUsingJwtAuthentication/Startup.cs
@@ -1,20 +1,20 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
+using SecuringWebApiUsingJwtAuthentication.Handlers;
+using SecuringWebApiUsingJwtAuthentication.Helpers;
 using SecuringWebApiUsingJwtAuthentication.IServices;
 using SecuringWebApiUsingJwtAuthentication.Models;
+using SecuringWebApiUsingJwtAuthentication.Requirements;
 using SecuringWebApiUsingJwtAuthentication.Services;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace SecuringWebApiUsingJwtAuthentication
 {
@@ -30,7 +30,6 @@ namespace SecuringWebApiUsingJwtAuthentication
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
             services.AddDbContext<JwtAuthDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("CustomersDbConnectionString")));
             services.AddScoped<ICustomerService, CustomerService>();
@@ -38,8 +37,29 @@ namespace SecuringWebApiUsingJwtAuthentication
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "SecuringWebApiUsingJwtAuthentication", Version = "v1" });
             });
-        }
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                    .AddJwtBearer(options =>
+                    {
+                        options.TokenValidationParameters = new TokenValidationParameters
+                        {
+                            ValidateIssuer = true,
+                            ValidateAudience = true,
+                            ValidateIssuerSigningKey = true,
+                            ValidIssuer = TokenHelper.Issuer,
+                            ValidAudience = TokenHelper.Audience,
+                            IssuerSigningKey = new SymmetricSecurityKey(Convert.FromBase64String(TokenHelper.Secret))
+                        };
 
+                    }); 
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("OnlyNonBlockedCustomer", policy => {
+                    policy.Requirements.Add(new CustomerBlockedStatusRequirement(false));
+
+                });
+            }); 
+            services.AddSingleton<IAuthorizationHandler, CustomerBlockedStatusHandler>();
+        }
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
@@ -49,13 +69,10 @@ namespace SecuringWebApiUsingJwtAuthentication
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "SecuringWebApiUsingJwtAuthentication v1"));
             }
-
+            app.UseAuthentication();
             app.UseHttpsRedirection();
-
             app.UseRouting();
-
             app.UseAuthorization();
-
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
