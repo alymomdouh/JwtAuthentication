03f772536f2c89bc2c993f578b9d5ef5393e6a20
alyxmomdouh@gmail.com <alyxmomdouh@gmail.com>
2023-01-16 01:33:16 +0200

    add order controller and interface and service class  and register

---
+++ SecuringWebApiUsingJwtAuthentication/Services/OrderService.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SecuringWebApiUsingJwtAuthentication.IServices;
+using SecuringWebApiUsingJwtAuthentication.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SecuringWebApiUsingJwtAuthentication.Services
+{
+    public class OrderService : IOrderService
+    {
+        private readonly JwtAuthDbContext customersDbContext; 
+        public OrderService(JwtAuthDbContext customersDbContext)
+        {
+            this.customersDbContext = customersDbContext;
+        }
+        public async Task<List<Order>> GetOrdersByCustomerId(int id)
+        {
+            var orders = await customersDbContext.Order.Where(order => order.CustomerId == id).ToListAsync(); 
+            return orders;
+        } 
+    }
+}
--- SecuringWebApiUsingJwtAuthentication/Startup.cs
+++ SecuringWebApiUsingJwtAuthentication/Startup.cs
@@ -30,9 +30,9 @@ namespace SecuringWebApiUsingJwtAuthentication
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
-            services.AddDbContext<JwtAuthDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("CustomersDbConnectionString")));
+             services.AddDbContext<JwtAuthDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("CustomersDbConnectionString")));
             services.AddScoped<ICustomerService, CustomerService>();
+            services.AddScoped<IOrderService, OrderService>();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "SecuringWebApiUsingJwtAuthentication", Version = "v1" });
@@ -59,6 +59,7 @@ namespace SecuringWebApiUsingJwtAuthentication
                 });
             }); 
             services.AddSingleton<IAuthorizationHandler, CustomerBlockedStatusHandler>();
+            services.AddControllers();
         }
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
